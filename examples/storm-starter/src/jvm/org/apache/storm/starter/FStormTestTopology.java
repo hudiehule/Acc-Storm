bf129338af17af08eb2ae8af8430ab5a394edd4b
hudiehule <1106862480@qq.com>
2018-10-24 20:02:40 +0800

    fstormtopology

--- examples/storm-starter/src/jvm/org/apache/storm/starter/FStormTestTopology.java
+++ examples/storm-starter/src/jvm/org/apache/storm/starter/FStormTestTopology.java
@@ -45,59 +45,7 @@ public class FStormTestTopology {
             this.time = time;
         }
     }
-    public static class C {
-        LocalCluster _local = null;
-        Nimbus.Client _client = null;
 
-        public C(Map conf) {
-            Map clusterConf = Utils.readStormConfig();
-            if (conf != null) {
-                clusterConf.putAll(conf);
-            }
-            Boolean isLocal = (Boolean)clusterConf.get("run.local");
-            if (isLocal != null && isLocal) {
-                _local = new LocalCluster();
-            } else {
-                _client = NimbusClient.getConfiguredClient(clusterConf).getClient();
-            }
-        }
-
-        public ClusterSummary getClusterInfo() throws Exception {
-            if (_local != null) {
-                return _local.getClusterInfo();
-            } else {
-                return _client.getClusterInfo();
-            }
-        }
-
-        public TopologyInfo getTopologyInfo(String id) throws Exception {
-            if (_local != null) {
-                return _local.getTopologyInfo(id);
-            } else {
-                return _client.getTopologyInfo(id);
-            }
-        }
-
-        public void killTopologyWithOpts(String name, KillOptions opts) throws Exception {
-            if (_local != null) {
-                _local.killTopologyWithOpts(name, opts);
-            } else {
-                _client.killTopologyWithOpts(name, opts);
-            }
-        }
-
-        public void submitTopology(String name, Map stormConf, StormTopology topology) throws Exception {
-            if (_local != null) {
-                _local.submitTopology(name, stormConf, topology);
-            } else {
-                StormSubmitter.submitTopology(name, stormConf, topology);
-            }
-        }
-
-        public boolean isLocal() {
-            return _local != null;
-        }
-    }
     public static class DataSpout extends BaseRichSpout {
         SpoutOutputCollector _collector;
         long _periodNano;
@@ -105,7 +53,6 @@ public class FStormTestTopology {
         Random _rand;
         long _nextEmitTime;
         long _emitsLeft;
-        HistogramMetric _histo;
         private static final String[] CHOICES = {
                 "marry had a little lamb whos fleese was white as snow marry had a little lamb whos fleese was white as snow",
                 "and every where that marry went the lamb was sure to go marry had a little lamb whos fleese was white as snow",
@@ -128,8 +75,6 @@ public class FStormTestTopology {
             _rand = ThreadLocalRandom.current();
             _nextEmitTime = System.nanoTime();
             _emitsLeft = _emitAmount;
-            _histo = new HistogramMetric(3600000000000L, 3);
-            context.registerMetric("comp-lat-histo", _histo, 10); //Update every 10 seconds, so we are not too far behind
         }
 
         @Override
@@ -141,21 +86,19 @@ public class FStormTestTopology {
 
             if (_emitsLeft > 0) {
                 String sentence = CHOICES[_rand.nextInt(CHOICES.length)];
-                _collector.emit(new Values(sentence), new SentWithTime(sentence, _nextEmitTime - _periodNano));
+                _collector.emit(new Values(sentence), sentence);
                 _emitsLeft--;
             }
         }
         @Override
         public void ack(Object id) {
             //Ignored
-            long end = System.nanoTime();
-            SentWithTime st = (SentWithTime)id;
-            _histo.recordValue(end-st.time);
+
+
         }
         @Override
         public void fail(Object id) {
-            SentWithTime st = (SentWithTime)id;
-            _collector.emit(new Values(st.sentence), id);
+            _collector.emit(new Values(id), id);
         }
 
         @Override
@@ -230,6 +173,8 @@ public class FStormTestTopology {
         public void declareOutputFields(OutputFieldsDeclarer declarer){
         }
         public void execute(Tuple tuple){
+            int value = tuple.getInteger(0);
+            System.out.println("result: " + value);
             _collector.ack(tuple);
         }
         public void cleanup(){
@@ -237,105 +182,6 @@ public class FStormTestTopology {
         }
     }
 
-    private static class MemMeasure {
-        private long _mem = 0;
-        private long _time = 0;
-
-        public synchronized void update(long mem) {
-            _mem = mem;
-            _time = System.currentTimeMillis();
-        }
-
-        public synchronized long get() {
-            return isExpired() ? 0l : _mem;
-        }
-
-        public synchronized boolean isExpired() {
-            return (System.currentTimeMillis() - _time) >= 20000;
-        }
-    }
-    private static final Histogram _histo = new Histogram(3600000000000L, 3);
-    private static final AtomicLong _systemCPU = new AtomicLong(0);
-    private static final AtomicLong _userCPU = new AtomicLong(0);
-    private static final AtomicLong _gcCount = new AtomicLong(0);
-    private static final AtomicLong _gcMs = new AtomicLong(0);
-    private static final ConcurrentHashMap<String, MemMeasure> _memoryBytes = new ConcurrentHashMap<String, MemMeasure>();
-    private static long _prev_acked = 0;
-    private static long _prev_uptime = 0;
-
-    private static long readMemory() {
-        long total = 0;
-        for (MemMeasure mem: _memoryBytes.values()) {
-            total += mem.get();
-        }
-        return total;
-    }
-
-    public static void printMetrics(C client, String name) throws Exception {
-        ClusterSummary summary = client.getClusterInfo();
-        String id = null;
-        for (TopologySummary ts: summary.get_topologies()) {
-            if (name.equals(ts.get_name())) {
-                id = ts.get_id();
-            }
-        }
-        if (id == null) {
-            throw new Exception("Could not find a topology named "+name);
-        }
-        TopologyInfo info = client.getTopologyInfo(id);
-        int uptime = info.get_uptime_secs();
-        long acked = 0;
-        long failed = 0;
-        for (ExecutorSummary exec: info.get_executors()) {
-            if ("spout".equals(exec.get_component_id()) && exec.get_stats() != null && exec.get_stats().get_specific() != null) {
-                SpoutStats stats = exec.get_stats().get_specific().get_spout();
-                Map<String, Long> failedMap = stats.get_failed().get(":all-time");
-                Map<String, Long> ackedMap = stats.get_acked().get(":all-time");
-                if (ackedMap != null) {
-                    for (String key: ackedMap.keySet()) {
-                        if (failedMap != null) {
-                            Long tmp = failedMap.get(key);
-                            if (tmp != null) {
-                                failed += tmp;
-                            }
-                        }
-                        long ackVal = ackedMap.get(key);
-                        acked += ackVal;
-                    }
-                }
-            }
-        }
-        long ackedThisTime = acked - _prev_acked;
-        long thisTime = uptime - _prev_uptime;
-        long nnpct, nnnpct, min, max;
-        double mean, stddev;
-        synchronized(_histo) {
-            nnpct = _histo.getValueAtPercentile(99.0);
-            nnnpct = _histo.getValueAtPercentile(99.9);
-            min = _histo.getMinValue();
-            max = _histo.getMaxValue();
-            mean = _histo.getMean();
-            stddev = _histo.getStdDeviation();
-            _histo.reset();
-        }
-        long user = _userCPU.getAndSet(0);
-        long sys = _systemCPU.getAndSet(0);
-        long gc = _gcMs.getAndSet(0);
-        double memMB = readMemory() / (1024.0 * 1024.0);
-        System.out.printf("uptime: %,4d acked: %,9d acked/sec: %,10.2f failed: %,8d " +
-                        "99%%: %,15d 99.9%%: %,15d min: %,15d max: %,15d mean: %,15.2f " +
-                        "stddev: %,15.2f user: %,10d sys: %,10d gc: %,10d mem: %,10.2f\n",
-                uptime, ackedThisTime, (((double)ackedThisTime)/thisTime), failed, nnpct, nnnpct,
-                min, max, mean, stddev, user, sys, gc, memMB);
-        _prev_uptime = uptime;
-        _prev_acked = acked;
-    }
-
-    public static void kill(C client, String name) throws Exception {
-        KillOptions opts = new KillOptions();
-        opts.set_wait_secs(0);
-        client.killTopologyWithOpts(name, opts);
-    }
 
     public static void main(String[] args) throws Exception{
         if(args == null ||args.length <8){
@@ -353,68 +199,6 @@ public class FStormTestTopology {
             boolean isDebug = Boolean.valueOf(args[7]);
 
             Config conf = new Config();
-            HttpForwardingMetricsServer metricServer = new HttpForwardingMetricsServer(conf) {
-                @Override
-                public void handle(IMetricsConsumer.TaskInfo taskInfo, Collection<IMetricsConsumer.DataPoint> dataPoints) {
-                    String worker = taskInfo.srcWorkerHost + ":" + taskInfo.srcWorkerPort;
-                    for (IMetricsConsumer.DataPoint dp: dataPoints) {
-                        if ("comp-lat-histo".equals(dp.name) && dp.value instanceof Histogram) {
-                            synchronized(_histo) {
-                                _histo.add((Histogram)dp.value);
-                            }
-                        } else if ("CPU".equals(dp.name) && dp.value instanceof Map) {
-                            Map<Object, Object> m = (Map<Object, Object>)dp.value;
-                            Object sys = m.get("sys-ms");
-                            if (sys instanceof Number) {
-                                _systemCPU.getAndAdd(((Number)sys).longValue());
-                            }
-                            Object user = m.get("user-ms");
-                            if (user instanceof Number) {
-                                _userCPU.getAndAdd(((Number)user).longValue());
-                            }
-                        } else if (dp.name.startsWith("GC/") && dp.value instanceof Map) {
-                            Map<Object, Object> m = (Map<Object, Object>)dp.value;
-                            Object count = m.get("count");
-                            if (count instanceof Number) {
-                                _gcCount.getAndAdd(((Number)count).longValue());
-                            }
-                            Object time = m.get("timeMs");
-                            if (time instanceof Number) {
-                                _gcMs.getAndAdd(((Number)time).longValue());
-                            }
-                        } else if (dp.name.startsWith("memory/") && dp.value instanceof Map) {
-                            Map<Object, Object> m = (Map<Object, Object>)dp.value;
-                            Object val = m.get("usedBytes");
-                            if (val instanceof Number) {
-                                MemMeasure mm = _memoryBytes.get(worker);
-                                if (mm == null) {
-                                    mm = new MemMeasure();
-                                    MemMeasure tmp = _memoryBytes.putIfAbsent(worker, mm);
-                                    mm = tmp == null ? mm : tmp;
-                                }
-                                mm.update(((Number)val).longValue());
-                            }
-                        }
-                    }
-                }
-            };
-            metricServer.serve();
-            String url = metricServer.getUrl();
-
-            C cluster = new C(conf);
-            conf.setNumWorkers(numWorkers);
-            conf.registerMetricsConsumer(org.apache.storm.metric.LoggingMetricsConsumer.class);
-            conf.registerMetricsConsumer(org.apache.storm.metric.HttpForwardingMetricsConsumer.class, url, 1);
-            Map<String, String> workerMetrics = new HashMap<String, String>();
-            if (!cluster.isLocal()) {
-                //sigar uses JNI and does not work in local mode
-                workerMetrics.put("CPU", "org.apache.storm.metrics.sigar.CPUMetric");
-            }
-            conf.put(Config.TOPOLOGY_WORKER_METRICS, workerMetrics);
-            conf.put(Config.TOPOLOGY_BUILTIN_METRICS_BUCKET_SIZE_SECS, 10);
-            conf.put(Config.TOPOLOGY_WORKER_GC_CHILDOPTS,
-                    "-XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:NewSize=128m -XX:CMSInitiatingOccupancyFraction=70 -XX:-CMSConcurrentMTEnabled");
-            conf.put(Config.TOPOLOGY_WORKER_CHILDOPTS, "-Xmx2g");
 
             TopologyBuilder builder = new TopologyBuilder();
 
@@ -432,17 +216,19 @@ public class FStormTestTopology {
             builder.setTopologyKernelFile(aoclFileName);//设置kernel本地可执行文件的路径 这个kernel必须是事先编译好的 提供kernel名称就可以了 去找
             String name = "FStormTestTopology"; //拓扑名称
 
-            try {
-                cluster.submitTopology(name, conf, builder.createTopology());
+            StormSubmitter.submitTopologyWithProgressBar(name, conf, builder.createTopology());
 
-                for (int i = 0; i < 20 * 2; i++) {
-                    Thread.sleep(30 * 1000);
-                    printMetrics(cluster, name);
-                }
-            } finally {
-                kill(cluster, name);
-            }
-            System.exit(0);
+
+            Map clusterConf = Utils.readStormConfig();
+            clusterConf.putAll(Utils.readCommandLineOpts());
+            Nimbus.Client client = NimbusClient.getConfiguredClient(clusterConf).getClient();
+
+            Thread.sleep(1000*60 * 10); //运行十分钟
+            //kill the topology
+            KillOptions opts = new KillOptions();
+            opts.set_wait_secs(0);
+            client.killTopologyWithOpts(name, opts);
+
         }
 
     }
