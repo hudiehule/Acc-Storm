6fec2a2cf04e31fb8eecc02303cd8b52aa4f3480
hudiehule <1106862480@qq.com>
2018-11-20 16:54:05 +0800

    constant parameter class

--- storm-core/src/jvm/org/apache/storm/topology/accelerate/ConstantParameter.java
+++ storm-core/src/jvm/org/apache/storm/topology/accelerate/ConstantParameter.java
@@ -1,10 +1,12 @@
 package org.apache.storm.topology.accelerate;
 
 
+import java.io.Serializable;
+
 /**
  * Created by Administrator on 2018/11/20.
  */
-public class ConstantParameter {
+public class ConstantParameter implements Serializable {
     public DataType type; // 数据类型，是枚举类型
     public String value; // 值 用字符串的形式存储
     public ConstantParameter(DataType type,int value){
